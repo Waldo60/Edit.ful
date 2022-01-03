1734d5a03683e4d852d57f85b28f2f356441b4b5
Waldo60 <80797901+Waldo60@users.noreply.github.com>
2022-01-03 02:20:42 -0400

    Update VPS-MX.sh

--- Install/VPS-MX.sh
+++ Install/VPS-MX.sh
@@ -258,7 +258,7 @@ echo 'echo -e "\t\033[97mPARA MOSTAR PANEL BASH ESCRIBA: sudo VPS-MX o vps-mx "'
 echo 'wget -O /etc/versin_script_new https://raw.githubusercontent.com/Waldo60/Edit.full/main/Install/Version &>/dev/null'>> .bashrc
 echo 'echo ""'>> .bashrc
 echo -e "         COMANDO PRINCIPAL PARA ENTRAR AL PANEL "
-echo -e "  \033[1;41m               sudo VPS-MX o vps-mx             \033[0;37m" && msg -bar2
+echo -e "  \033[1;41m               Menu o vps-mx             \033[0;37m" && msg -bar2
 sleep 5
 exit
 }
