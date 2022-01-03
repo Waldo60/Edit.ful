603fb01efcfec245639c333de248585cdd2915ab
Waldo60 <80797901+Waldo60@users.noreply.github.com>
2022-01-02 22:03:18 -0400

    Update VPS-MX.sh

--- Install/VPS-MX.sh
+++ Install/VPS-MX.sh
@@ -244,11 +244,11 @@ echo "exit 0" >> /etc/rc.local
 /bin/cp /etc/skel/.bashrc ~/
 echo 'clear' >> .bashrc
 echo 'echo ""' >> .bashrc
-echo 'echo -e "\t\033[91m __     ______  ____        __  ____ " '>> .bashrc
-echo 'echo -e "\t\033[91m \ \   / /  _ \/ ___|      |  \/  \  " '>> .bashrc
-echo 'echo -e "\t\033[91m  \ \ / /| |_) \___ \ _____| |\/| |\ " '>> .bashrc
-echo 'echo -e "\t\033[91m   \ V / |  __/ ___) |_____| |  | |/ " '>> .bashrc
-echo 'echo -e "\t\033[91m    \_/  |_|   |____/      |_|  |_/_ " '>> .bashrc
+echo 'echo -e "\t\033[91m __     ______  ____        __  __  ___ " '>> .bashrc
+echo 'echo -e "\t\033[91m \ \   / /  _ \/ ___|      | |  | |  -  |" '>> .bashrc
+echo 'echo -e "\t\033[91m  \ \ / /| |_) \___ \ _____| |/\| | | | |" '>> .bashrc
+echo 'echo -e "\t\033[91m   \ V / |  __/ ___) |_____|  /\  | |-| |" '>> .bashrc
+echo 'echo -e "\t\033[91m    \_/  |_|   |____/      |_/  \_|\____/" '>> .bashrc
 echo 'echo "" '>> .bashrc
 echo 'mess1="$(less /etc/VPS-MX/message.txt)" ' >> .bashrc
 echo 'echo "" '>> .bashrc
